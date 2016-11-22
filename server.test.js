a07f19705e7b93079595dceead6cdd4a8ad0b314
Jeremy Lu <jatwork@twmug.com>
2016-11-22 16:15:29 +0800

    err

--- server.test.js
+++ server.test.js
@@ -5,7 +5,7 @@ import server from './server'
 describe( 'see all these tests running and passing', () => {
 
 	it('adds 1 + 2 to equal 3', () => {
-	  expect( 1 + 2 ).toBe( 3 )
+	  expect( 1 + 2 ).toBe( 13 )
 	})
 
 	it( 'run 2nd test alright', () => {
